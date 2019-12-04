b9a77665cf57fa6900a0db540311e141fa4fda78
nopjmp <jmp@0x.gg>
2019-12-03 19:00:00 -0600

    Refactor/Rewrite XmlRpcRequest to use HttpClient
    While we are here, fix some weirdness and spelling mistakes.

---
+++ XmlRpcCore/XmlRpcHttpClientExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlRpcCore
+{
+    public static class XmlRpcHttpClientExtensions
+    {
+        private static readonly XmlRpcResponseDeserializer Deserializer = new XmlRpcResponseDeserializer();
+        private static readonly XmlRpcRequestSerializer Serializer = new XmlRpcRequestSerializer();
+        
+        /// <summary>Send a XmlRpcRequest to the server.</summary>
+        /// <param name="client"><c>HttpClient</c> HttpClient used to submit the request</param>
+        /// <param name="requestUri"><c>String</c> The uri of the XML-RPC server.</param>
+        /// <param name="rpcRequest"><c>XmlRpcRequest</c> The rpc request object to send</param>
+        /// <returns><c>XmlRpcResponse</c> The response generated.</returns>
+        public static async Task<XmlRpcResponse> PostAsXmlRpcAsync(this HttpClient client, string requestUri, XmlRpcRequest rpcRequest)
+        {
+            await using var stream = new MemoryStream();
+            using (var writer = new XmlTextWriter(stream, Encoding.ASCII))
+            {
+                Serializer.Serialize(writer, rpcRequest);
+            }
+
+            var request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(requestUri),
+                Content = new StreamContent(stream)
+            };
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("text/xml");
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            await using var contentStream = await response.Content.ReadAsStreamAsync();
+            using var streamReader = new StreamReader(contentStream);
+            return (XmlRpcResponse)Deserializer.Deserialize(streamReader);
+        }
+    }
+}
--- XmlRpcCore/XmlRpcRequest.cs
+++ XmlRpcCore/XmlRpcRequest.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using System.Xml;
 
 namespace XmlRpcCore
@@ -10,18 +12,13 @@ namespace XmlRpcCore
     /// <summary>Class supporting the request side of an XML-RPC transaction.</summary>
     public class XmlRpcRequest
     {
-        private readonly XmlRpcResponseDeserializer _deserializer = new XmlRpcResponseDeserializer();
-        private readonly Encoding _encoding = new ASCIIEncoding();
-
-        /// <summary><c>ArrayList</c> containing the parameters.</summary>
-        protected IList _params;
-
-        private readonly XmlRpcRequestSerializer _serializer = new XmlRpcRequestSerializer();
+        private readonly Lazy<XmlRpcRequestSerializer> _serializer =
+            new Lazy<XmlRpcRequestSerializer>(() => new XmlRpcRequestSerializer());
 
         /// <summary>Instantiate an <c>XmlRpcRequest</c></summary>
         public XmlRpcRequest()
         {
-            _params = new ArrayList();
+            Params = new ArrayList();
         }
 
         /// <summary>Instantiate an <c>XmlRpcRequest</c> for a specified method and parameters.</summary>
@@ -32,18 +29,22 @@ namespace XmlRpcCore
         /// <param name="parameters"><c>ArrayList</c> of XML-RPC type parameters to invoke the request with.</param>
         public XmlRpcRequest(string methodName, IList parameters)
         {
-            MethodName = methodName;
-            _params = parameters;
+            _methodName = methodName;
+            Params = parameters;
         }
 
-        /// <summary><c>ArrayList</c> conntaining the parameters for the request.</summary>
-        public virtual IList Params
+        // XXX: workaround virtual method call in constructor
+        private string _methodName = "";
+
+        /// <summary><c>ArrayList</c> containing the parameters for the request.</summary>
+        public virtual IList Params { get; }
+
+        /// <summary><c>String</c> containing the method name, both object and method, that the request will be sent to.</summary>
+        public virtual string MethodName
         {
-            get { return _params; }
+            get => _methodName;
+            set => _methodName = value;
         }
-
-        /// <summary><c>String</c> conntaining the method name, both object and method, that the request will be sent to.</summary>
-        public virtual string MethodName { get; set; }
 
         /// <summary><c>String</c> object name portion of the method name.</summary>
         public string MethodNameObject
@@ -74,12 +75,13 @@ namespace XmlRpcCore
         }
 
         /// <summary>Invoke this request on the server.</summary>
+        /// <param name="client"><c>HttpClient</c> HttpClient object to use for the request.</param>
         /// <param name="url"><c>String</c> The url of the XML-RPC server.</param>
         /// <returns><c>Object</c> The value returned from the method invocation on the server.</returns>
         /// <exception cref="XmlRpcException">If an exception generated on the server side.</exception>
-        public object Invoke(string url)
+        public async Task<object> Invoke(HttpClient client, string url)
         {
-            var res = Send(url);
+            var res = await client.PostAsXmlRpcAsync(url, this);
 
             if (res.IsFault)
                 throw new XmlRpcException(res.FaultCode, res.FaultString);
@@ -87,42 +89,11 @@ namespace XmlRpcCore
             return res.Value;
         }
 
-        /// <summary>Send the request to the server.</summary>
-        /// <param name="url"><c>String</c> The url of the XML-RPC server.</param>
-        /// <param name="timeout">AMount of time in milliseconds to wait for timeout</param>
-        /// <returns><c>XmlRpcResponse</c> The response generated.</returns>
-        public XmlRpcResponse Send(string url, int timeout = 0)
-        {
-            var request = (HttpWebRequest) WebRequest.Create(url);
-            if (request == null)
-                throw new XmlRpcException(XmlRpcErrorCodes.TRANSPORT_ERROR,
-                    XmlRpcErrorCodes.TRANSPORT_ERROR_MSG + ": Could not create request with " + url);
-            request.Method = "POST";
-            request.ContentType = "text/xml";
-            request.AllowWriteStreamBuffering = true;
-            if (timeout > 0)
-                request.Timeout = timeout;
-
-            var stream = request.GetRequestStream();
-            var xml = new XmlTextWriter(stream, _encoding);
-            _serializer.Serialize(xml, this);
-            xml.Flush();
-            xml.Close();
-
-            var response = (HttpWebResponse) request.GetResponse();
-            var input = new StreamReader(response.GetResponseStream());
-
-            var resp = (XmlRpcResponse) _deserializer.Deserialize(input);
-            input.Close();
-            response.Close();
-            return resp;
-        }
-
         /// <summary>Produce <c>String</c> representation of the object.</summary>
         /// <returns><c>String</c> representation of the object.</returns>
         public override string ToString()
         {
-            return _serializer.Serialize(this);
+            return _serializer.Value.Serialize(this);
         }
     }
 }
